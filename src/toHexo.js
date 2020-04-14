8c27e1f846974f92fa600ad830ef957f99c1d379
cyang39 <huqinyang123@gmail.com>
2020-04-15 00:32:58 +0800

    fix bug

--- src/toHexo.js
+++ src/toHexo.js
@@ -7,7 +7,7 @@ module.exports = function (raw) {
   const body = post.body
   delete post.body
   const catRegArr = /\[(.*)\](.*)/.exec(post.title)
-  if(catRegArr[0] === post.title) {
+  if(catRegArr && catRegArr[0] === post.title) {
     post.title = catRegArr[2]
     post.category = catRegArr[1]
   }
