94b64b375b3917326296d2e1507bf6a90db7d11f
cyang39 <huqinyang123@gmail.com>
2020-04-15 00:29:48 +0800

    add category feature

--- src/toHexo.js
+++ src/toHexo.js
@@ -3,7 +3,13 @@ const ehto = require('ever-html-to-object')
 module.exports = function (raw) {
   const post = ehto(raw, { rmAssetsPrefix: true })
   post.date = post.created
+  delete post.created
   const body = post.body
   delete post.body
+  const catRegArr = /\[(.*)\](.*)/.exec(post.title)
+  if(catRegArr[0] === post.title) {
+    post.title = catRegArr[2]
+    post.category = catRegArr[1]
+  }
   return ["---", yaml.safeDump(post), "---", body].join('\n')
 }
