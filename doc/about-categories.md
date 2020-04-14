a1fe4f94074b84a9088f1e1a7c52e6516e9ee07c
cyang39 <huqinyang123@gmail.com>
2020-04-15 00:42:51 +0800

    add category

---
+++ doc/about-categories.md
@@ -0,0 +1,7 @@
+# 关于分类
+
+不少的主题支持分类，比如 [Icarus](https://blog.zhangruipeng.me/hexo-theme-icarus/)。
+
+此插件通过笔记名称前缀对笔记分类，
+
+例如，标题`[foo]bar`，会被分类为`foo`，笔记的标题为`bar`。
--- src/toHexo.js
+++ src/toHexo.js
@@ -1,5 +1,8 @@
 const yaml = require('js-yaml')
 const ehto = require('ever-html-to-object')
+/**
+ * @param {String} raw
+ */
 module.exports = function (raw) {
   const post = ehto(raw, { rmAssetsPrefix: true })
   post.date = post.created
@@ -8,8 +11,8 @@ module.exports = function (raw) {
   delete post.body
   const catRegArr = /\[(.*)\](.*)/.exec(post.title)
   if(catRegArr && catRegArr[0] === post.title) {
-    post.title = catRegArr[2]
-    post.category = catRegArr[1]
+    post.title = catRegArr[2].trim()
+    post.category = catRegArr[1].trim()
   }
   return ["---", yaml.safeDump(post), "---", body].join('\n')
 }
